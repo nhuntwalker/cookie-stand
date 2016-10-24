4397eead04baa6b68a40569a4cbedf43891a6f36
Geno Guerrero <geno.guerrero@gmail.com>
2016-10-24 15:39:41 -0700

    building objects not finished

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var firstAndPike = {
+    location = '1st and Pike';
+    minHourly = 23;
+    maxHourly = 65;
+    avgCookieSale = 6.3;
+}
+
+var seatac = {
+    location = 'SeaTac Airport';
+    minHourly = 3;
+    maxHourly = 24;
+    avgCookieSale = 1.2;
+}
+
+var seattleCent = {
+    location = 'Seattle Center';
+    minHourly = 11;
+    maxHourly = 38;
+    avgCookieSale = 3.7;
+}
+
+var capHill = {
+    location = 'Capitol Hill';
+    minHourly = 20;
+    maxHourly = 38;
+    avgCookieSale = 2.3;
+}
+
+var alki = {
+    location = 'Alki';
+    minHourly = 2;
+    maxHourly = 16;
+    avgCookieSale = 4.6;
+}
