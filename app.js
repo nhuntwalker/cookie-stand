0b8cd97e7658db62f11553b7a347efb34859c238
Geno Guerrero <geno.guerrero@gmail.com>
2016-10-27 08:43:50 -0700

    cleaned up code still need to fix last box.

--- app.js
+++ app.js
@@ -21,24 +21,7 @@ function CookieStore (location, minHourly, maxHourly, avgSale) {
       this.storeTotals += this.totalDailySales[i];
     }
   };
-  // attach store name to a h2 header
-  this.attachStoreName = function () {
-    var h2 = document.createElement('h2');
-    h2.innerText = (this.location);
-    var content = document.getElementById('content');
-    content.appendChild(h2);
-  };
-  //function to generate the amount of cookies per hour
-  this.createStoreInfo = function () {
-    var ul = document.createElement('ul');
-    for (var i = 0; i < hours.length; i++) {
-      var li = document.createElement('li');
-      li.innerText = hours[i] + ': ' + this.totalDailySales[i] + ' cookies';
-      ul.appendChild(li);
-    }
-    var content = document.getElementById('content');
-    content.appendChild(ul);
-  };
+
   // create a row of data for each store
   this.createTableRow = function() {
     var table = document.getElementById('store-data');
@@ -112,10 +95,10 @@ function createTotalRow() {
   var th = document.createElement('th');
   th.innerText = 'Total: ';
   tr.appendChild(th);
-  for (var i = 1; i < hours.length; i++) {
+  for (var i = 0; i < hours.length; i++) {
     var hourlySales = 0;
     var hourlyStoreTotal = 0;
-    for (var j = 1; j < locations.length; j++) {
+    for (var j = 0; j < locations.length; j++) {
       hourlySales += locations[j].totalDailySales[i];
     }
     var td = document.createElement('td');
